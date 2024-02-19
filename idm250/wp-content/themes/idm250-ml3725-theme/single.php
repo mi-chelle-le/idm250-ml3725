a8f2bfe1c287fa4d59e26f71f2de828b742888f0
mi-chelle-le <ml3725@drexel.edu>
2024-02-19 02:47:25 -0500

    Update single.php

--- idm250/wp-content/themes/idm250-ml3725-theme/single.php
+++ idm250/wp-content/themes/idm250-ml3725-theme/single.php
@@ -1,6 +1,11 @@
 <?php get_header(); ?>
-<h1>
+<!-- <h1>
   <?php echo get_the_title(); ?>
-</h1>
-<p>This is a single.php template</p>
+</h1> -->
+<div class="content">
+  <?php
+    echo get_the_content();
+  ?>
+</div>
+<!-- <p>This is a single.php template</p> -->
 <?php get_footer(); ?>

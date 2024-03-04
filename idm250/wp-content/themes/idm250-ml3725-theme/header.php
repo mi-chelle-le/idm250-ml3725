6fcf1d64c656f8c16ffcceaf1b5e7825a07aaded
mi-chelle-le <ml3725@drexel.edu>
2024-03-03 21:35:52 -0500

    Beta updates

--- idm250/wp-content/themes/idm250-ml3725-theme/header.php
+++ idm250/wp-content/themes/idm250-ml3725-theme/header.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
+  <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/favicon.svg" />
   <?php wp_head(); ?>
 </head>
 
--- idm250/wp-content/themes/idm250-ml3725-theme/page.php
+++ idm250/wp-content/themes/idm250-ml3725-theme/page.php
@@ -1,8 +1,15 @@
+<?php /* Template Name: Default Template */?>
+
 <?php get_header(); ?>
 <div class="body-container">
 <h1>
-  <?php echo get_the_title(); ?>
+  <?php echo get_the_title(); ?>, Heading 1
 </h1>
-<p>This is a page.php template</p>
+<!-- <p>This is a page.php template</p> -->
+<div class="content default">
+  <?php
+    echo get_the_content();
+  ?>
+</div>
 </div>
 <?php get_footer(); ?>
